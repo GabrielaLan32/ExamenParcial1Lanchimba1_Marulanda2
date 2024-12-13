aabc6108bcb3b7139b28ed21f0cffa00b109053a
Marulanda6 <nahim.marulanda686@ist17dejulio.edu.ec>
2024-12-13 11:27:24 -0500

    Clases Libro y Prestamo

---
+++ ExamenParcial1Lanchimba1_Marulanda2/src/modelo/Libro.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+/**
+ *
+ * @author kirlok6
+ */
+public class Libro implements Prestamo {
+    private String titulo;
+    private int codigo;
+    private int anioPublicacion;
+   
+   
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public int getAnioPublicacion() {
+        return anioPublicacion;
+    }
+
+    public void setAnioPublicacion(int anioPublicacion) {
+        this.anioPublicacion = anioPublicacion;
+    }
+
+   
+
+    @Override
+    public boolean disponibilidad() {
+        return true; // Implementar lógica real de disponibilidad
+    }
+
+}
+
+
